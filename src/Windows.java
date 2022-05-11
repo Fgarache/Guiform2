1fec82b189a35751da8e4519d02ab892d32b058e
Francisco Garache <franciscogarach3@gmail.com>
2022-05-11 10:49:39 -0600

    Initial commit

---
+++ src/Windows.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Windows extends JFrame{
+    private JPanel panel1;
+
+    public Windows(){
+        super("Windows");
+        setContentPane(panel1);
+
+    }
+
+}
